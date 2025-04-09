9a8b372d1844765df3fb9b3a0a5a1ac55ffd52c0
Sergio Machaca <SMachaca@hansa.com.bo>
2025-04-09 17:17:18 -0400

    Feat. cambios cors

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//  adding cors
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const tareasRouter = require('./routes/tareas');
 
 var app = express();
+// adding cors
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // Allow credentials (cookies, authorization headers, etc.)
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
