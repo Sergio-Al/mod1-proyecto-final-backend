b73429fc5cb425d36a34ed43392e91567026ad4f
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-04-10 22:30:23 -0400

    Feat. cors all

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-require('dotenv').config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 //  adding cors
-const cors = require('cors');
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const tareasRouter = require('./routes/tareas');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const tareasRouter = require("./routes/tareas");
 
 var app = express();
 // adding cors
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true // Allow credentials (cookies, authorization headers, etc.)
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/tareas', tareasRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/tareas", tareasRouter);
 
 module.exports = app;
